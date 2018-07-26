06e395b1d67fb0d0d26abad00f6a1d48d11a1835
John Huynh <john.huynh1996@gmail.com>
2018-07-26 23:53:54 +1000

    Initial Commit

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import "./styles/index.css";
+import App from "./components/App";
+import reducers from "./reducers";
+import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(
+	reducers /* preloadedState, */,
+	window.__REDUX_DEVTOOLS_EXTENSION__ &&
+		window.__REDUX_DEVTOOLS_EXTENSION__(),
+	applyMiddleware(promise)
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<div>
+			<App />
+		</div>
+	</Provider>,
+	document.getElementById("root")
+);
+registerServiceWorker();
